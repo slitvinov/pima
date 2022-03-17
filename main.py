94d8d710c247539abd1c472b82f11a7b73e1ca31
Sergey Litvinov <slitvinov@gmail.com>
2022-03-17 11:55:19 -0400

    up

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import arff
 
+X = [ ]
+y = [ ]
 for row in arff.load('pima.arff'):
-    x = row
-    print(x[0])
+    x = [ ]
+    n = len(row)
+    for i in range(n - 1):
+        x.append(row[i])
+    X.append(x)
+    y.append(0 if row[n - 1] == 'tested_negative' else 1)
+
+print(y)
